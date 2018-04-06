7cdd80600fc7c4e18efdeef841da2701bb83286e
elianaCvargas <carla.vargas.e@gmail.com>
2018-04-05 22:15:04 -0300

    Add files via upload

---
+++ Repaso/Repaso/Estante.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+    public class Estante
+    {
+        
+        private Producto[] listaProductos;
+        private int ubicacionEstante;
+
+        public Estante(int capacidad)
+            
+        {
+            listaProductos = new Producto[capacidad];
+        }
+        public Estante(int capacidad, int ubicacion)
+            :this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProducto {
+            get { return this.listaProductos; } 
+        }
+
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+            for(int i =0; i< e.listaProductos.Length; i++)
+            {
+              sb.Append(   Producto.MostrarProducto(e.listaProductos[i]));
+            }
+            return String.Format("Estante ubicacion: {0}\n Producto: {1}\n", e.ubicacionEstante, sb.ToString());
+        }
+
+        public static bool operator !=(Producto p, Estante e)
+        {
+            bool retorno = false;
+            for (int i = 0; i < e.listaProductos.Length; i++)
+            {
+                if (!(Object.ReferenceEquals(e.listaProductos[i], null)))
+                {
+                    if (p != e.listaProductos[i])
+                        retorno = true;
+                    return retorno = false;
+                }
+                
+            }
+            return retorno;
+
+        }
+
+        public static bool operator ==(Producto p, Estante e)
+        { 
+            return !(p != e);
+        }
+        public static bool operator +(Estante e, Producto p)
+        {
+            bool retorno = false;
+            for (int i = 0; i < e.listaProductos.Length; i++)
+            {
+                if (Object.ReferenceEquals(e.listaProductos[i], null) && p != e)
+                {
+                    e.listaProductos[i] = p;
+                    retorno = true;
+                }
+                retorno = false;
+            }
+            return retorno;
+        }
+
+        public static bool operator -(Estante e, Producto p)
+        { 
+            return !(e + p);
+        }
+
+
+    }
+}
---
+++ Repaso/Repaso/Producto.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto( string marca, string codigoBarra, float precio)
+        {
+            this.codigoDeBarra = codigoBarra;
+            this.marca = marca;
+            this.precio = precio;
+        }
+
+        //public string GetMarca
+        //{ get { return this.marca; } }
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+        //public float  GetPrecio
+        //{ get { return this.precio; } }
+
+        public static string MostrarProducto(Producto p) 
+        {
+            return String.Format("Codigo de Barra: {0}\n Marca: {1}\n Precio: {2}\n", p.codigoDeBarra, p.marca, p.precio);
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public static bool operator !=(Producto p, string marca)
+        {
+            if (p.marca == marca)
+                return true;
+            return false;
+        }
+
+        public static bool operator ==(Producto p, string marca)
+        {
+            return !(p.marca != marca);
+        }
+
+        public static bool operator ==(Producto p1, Producto p2 )
+        {
+            if (p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra)
+                return true;
+            return false;                           
+        }
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra);
+        }
+
+
+
+
+
+        
+    }
+}
